da7b5c1b4d898aa38b9b2a4097d148aacebcc232
Denis <ggmazanovdenis@gmail.com>
2023-06-19 22:30:36 +0300

    the end

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,8 @@
+// Hamburger
+
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
+      overlay = document.querySelector('.menu__overlay'),
       closeElem = document.querySelector('.menu__close');
 
     hamburger.addEventListener('click', () => {
@@ -10,9 +13,50 @@ const hamburger = document.querySelector('.hamburger'),
         menu.classList.remove('active');
     });
 
-const counters = document.querySelectorAll('.skills__rating-counter'),
-lines = document.querySelectorAll('.skills__rating-line span');
+    menu.addEventListener('click',(e) => {
+        if(e.target === overlay) {
+            menu.classList.remove('active');
+        }
+    });
 
-counters.forEach( (item, i) => {
+
+//Counters
+const counters = document.querySelectorAll('.skills__rating-counter'),
+      lines = document.querySelectorAll('.skills__rating-line span');
+
+counters.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
+});
+
+// Smooth scroll and page up
+
+const pageUp = document.querySelector('.pageup');
+function getScroll() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+	let windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+	let documentHeight = Math.max(
+		document.body.scrollHeight, document.body.offsetHeight, document.body.clientHeight,
+		document.documentElement.scrollHeight, document.documentElement.offsetHeight, document.documentElement.clientHeight);
+    
+        return Math.round(((scrollTop / (documentHeight - windowHeight)) * 100))
+}
+window.addEventListener('scroll', () => {
+    if(getScroll() > 30) {
+        pageUp.style.opacity = 1;
+    } else {
+        pageUp.style.opacity = 0;
+    }
+});
+
+const links = document.querySelectorAll('[data-smooth]');
+links.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const id = link.getAttribute('href');
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
 });
